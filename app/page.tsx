986b94848802ef0580253e37f429e0c8263a7399
lismalaj <150190778+lismalaj@users.noreply.github.com>
2024-01-10 10:48:38 -0500

    foldre 1

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Intro from '@/components/intro'
+import SectionDivider from '@/components/section-divider'
+import About from '@/components/about'
+import Image from 'next/image'
+import Projects from '@/components/projects'
+import Skills from '@/components/skills'
+import Contact from '@/components/contact'
+import Footer from '@/components/footer'
+
+export default function Home() {
+  return (
+    <main className=" flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <SectionDivider />
+      <SectionDivider />
+      <SectionDivider />
+      <Skills />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
